d77d5db60f54674e8b9ad3026e87c0b7f9093c9f
tpn1071 <tpn1071@gmail.com>
2025-05-23 02:10:49 +0700

    bổ sung flask mở port

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from threading import Thread
 
 load_dotenv()
 
@@ -21,8 +23,18 @@ cogs = [
     "cogs.help_suggest",
 ]
 
+def run_web():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "VNStars-bot is running!"
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
+    Thread(target=run_web).start()
     for cog in cogs:
         bot.load_extension(cog)
-
-bot.run(TOKEN)
+    bot.run(TOKEN)
