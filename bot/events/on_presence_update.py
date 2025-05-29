cf24e27724c8844130c05f9448bd917cb2264ecb
tpn1071 <tpn1071@gmail.com>
2025-05-29 18:59:53 +0700

    bổ sung print để khỏi bị lỗi khi chạy project

--- bot/events/on_presence_update.py
+++ bot/events/on_presence_update.py
@@ -5,3 +5,5 @@ from discord.ext import commands
 def setup_on_presence_update(bot: commands.Bot):
     @bot.event
     async def on_presence_update(before: discord.Member, after: discord.Member):  # type: ignore
+        # log check run
+        print("heh")
--- bot/events/on_reaction_add.py
+++ bot/events/on_reaction_add.py
@@ -5,3 +5,4 @@ from discord.ext import commands
 def setup_on_reaction_add(bot: commands.Bot):
     @bot.event
     async def on_reaction_add(reaction: discord.Reaction, user: discord.User):  # type: ignore
+        print(" ff")
